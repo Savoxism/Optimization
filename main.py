cb0ab0f3a693ea640e20892fe437d2e5f014108b
Phu An Nguyen <150226832+Savoxism@users.noreply.github.com>
2024-12-19 23:33:39 +0700

    update

--- main.py
+++ main.py
@@ -1,17 +1,164 @@
-import random
 
-def initialize_population(num_courses, num_teachers, population_size):
-    """Initialize population with random valid assignments."""
-    return [
-        [random.randint(0, num_teachers - 1) for _ in range(num_courses)]
-        for _ in range(population_size)
-    ]
+# x handler
+def check_x(vertex,k):
+    global Q
+    global d
 
-# Test the function
-num_courses = 5  # Number of courses
-num_teachers = 3  # Number of teachers
-population_size = 10  # Population size
+    if vertex > 0 and visited[vertex]:
+        return False
+    if load[k] + d[vertex] > Q:
+        return False
 
-# Generate the population
-population = initialize_population(num_courses, num_teachers, population_size)
-print(population)
+    return True
+        
+def backtracking_x(state,k):
+    global segments
+    global nbR
+    global Cmin
+    global f
+    global f_star
+    global K
+    global n
+    
+    
+    #điều kiện dừng trường hợp riêng x
+    if state == 0: # về depot đi cu
+        if k < K:
+            backtracking_x(y[k+1],k + 1) 
+            # đi tìm tiếp cho xe k + 1 
+        
+        return 
+
+    for vertex in range(0,n+1):
+        if  state != vertex:
+            if check_x(vertex,k):
+                
+                x[state] = vertex
+                
+                visited[vertex] = True
+                f = f + matrix[state][vertex]
+                load[k] += d[vertex]
+                segments = segments + 1
+                
+                # logic backtrack_x
+                
+                if vertex > 0:
+                    #real customer
+                    if f + (n + nbR - segments)* Cmin < f_star:
+                        backtracking_x(vertex,k)
+                else:
+                    if k == K:
+                        if segments == n + nbR:
+                            update_best()
+                    else:
+                        if f + (n + nbR - segments) * Cmin < f_star:
+                                backtracking_x(y[k+1],k+1)
+                
+                # trả lại value
+                
+                visited[vertex] = False
+                f -=  matrix[state][vertex]
+                load[k] = load[k] - d[vertex]
+                segments = segments - 1
+
+
+# cập nhật ngon nhất
+def update_best(): 
+    global f_star
+    global f
+    
+    if f  < f_star:
+        
+        f_star = f 
+# y handler
+
+def check_y(vertex,k):
+
+    global Q
+    global d
+    
+    if vertex == 0:
+        return True
+    if load[k] + d[vertex] > Q:
+        return False
+    if visited[vertex] == True:
+        return False
+    
+    return True
+
+#k là xe thứ k hiện tại
+def backtracking_y(k):
+    global segments
+    global nbR
+    global Cmin
+    global f
+    global f_star
+    global K
+    global n
+
+    s = 0
+    if y[k-1] > 0:
+        s = y[k-1] + 1
+    
+    for vertex in range(s,n+1):
+        if check_y(vertex,k):
+            y[k] = vertex
+            if vertex > 0:
+                segments = segments + 1
+            
+            visited[vertex] = True
+            f = f + matrix[0][vertex]
+            load[k] = load[k] + d[vertex]
+            
+            if k < K:
+                backtracking_y(k + 1)
+            else:
+                nbR = segments
+                backtracking_x(y[1],1)
+            
+            
+            # trả lại giá trị
+            if vertex > 0:
+                segments = segments - 1
+            
+            
+            load[k] = load[k] - d[vertex]
+            visited[vertex] = False
+            f = f - matrix[0][vertex]
+           
+
+              
+# solve handler 
+#input handler
+n,K,Q = (int(x) for x in input().split())
+d = [0] + [int (x) for x in input().split()]
+matrix = []
+for i in range(n + 1):
+    matrix.append([int(x) for x in input().split()])
+
+#logic variable handler
+
+x = [None] + [0 for i in range(n)]
+y = [None] + [0  for i in range(K)]
+
+
+load = [None] + [0 for i in range(K)]
+nbR = 0
+segments = 0
+# k = 1
+
+# tính Cmin
+Cmin = 10000
+for i in range(0,n + 1):
+    for j in range(0,n + 1):
+        if i != j:
+            Cmin = min(Cmin,matrix[i][j])
+Cmin += 0.95
+f = 0
+f_star = 10000
+
+y[0] = 0
+visited = [False]+[False for i in range(n)]
+
+backtracking_y(1)
+print(f_star)
--- two_disjoint_paths.py
+++ two_disjoint_paths.py
@@ -5,11 +5,76 @@ import sys
 def read_input():
     lines = sys.stdin.readlines()
     no_nodes, no_edges = map(int, lines[0].split())
-    
+
     edges = []
     for i in range(1, no_edges + 1):
         u, v, w = map(int, lines[i].split())
-        edges.append(u, v, w)
-        
+        edges.append((u, v, w))
+
     return no_nodes, edges
+
+
+def solve(no_nodes, edges, s, t):
+    model = cp_model.CpModel()
     
+    x = {}
+    y = {}
+    for u, v, _ in edges:
+        x[(u, v)] = model.NewBoolVar(f"x[{u},{v}]")
+        y[(u, v)] = model.NewBoolVar(f"y[{u},{v}]")
+        
+    # Constraint 1: flow conservation
+    for node in range(1, no_nodes + 1):
+        if node == s or node == t:
+            continue
+        
+        # First path flow conservation
+        model.Add(
+            sum(x[(u, v)] for u, v, _ in edges if u == node) ==
+            sum(x[(v, u)] for v, u, _ in edges if u == node)
+        )
+
+        # Second path flow conservation
+        model.Add(
+            sum(y[(u, v)] for u, v, _ in edges if u == node) ==
+            sum(y[(v, u)] for v, u, _ in edges if u == node)
+        )
+        
+    # Constraint 2: Start and end
+    model.Add(sum(x[(s, v)] for node, v, _ in edges if s == node) == 1)
+    model.Add(sum(y[(s, v)] for node, v, _ in edges if s == node) == 1)
+    model.Add(sum(x[(v, t)] for v, node, _ in edges if t == node) == 1)
+    model.Add(sum(y[(v, t)] for v, node, _ in edges if t == node) == 1)
+    
+    # Constraint 3: Disjoint paths
+    for u, v, w in edges:
+        model.Add(x[(u, v)] + y[(u, v)] <= 1)
+        
+    # Objective function
+    model.Minimize(
+        sum(w * x[(u, v)] for u, v, w in edges) +
+        sum(w * y[(u, v)] for u, v, w in edges)
+    )
+    
+    # Initalize solver
+    solver = cp_model.CpSolver()
+    status = solver.Solve(model)
+    
+    if status == cp_model.OPTIMAL:
+        print(int(solver.ObjectiveValue()))
+
+        # print("First path edges:")
+        # for u, v, _ in edges:
+        #     if solver.Value(x[(u, v)]) == 1:
+        #         print(f"({u}, {v})")
+
+        # print("Second path edges:")
+        # for u, v, _ in edges:
+        #     if solver.Value(y[(u, v)]) == 1:
+        #         print(f"({u}, {v})")
+    else:
+        print("NOT_FEASIBLE")
+        
+if __name__ == "__main__":
+    no_nodes, edges = read_input()
+    solve(no_nodes=no_nodes, edges=edges, s=1, t=no_nodes)
